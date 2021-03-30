daee1d41e2505ed02e97eb8c69a58cd9b8bb9be9
Noy Maday <Noy.Maday@cybereason.com>
2021-03-30 21:51:50 +0300

    Adding unit tests to trip service

--- src/main/java/com/cybereason/trip_finder/user/User.java
+++ src/main/java/com/cybereason/trip_finder/user/User.java
@@ -10,11 +10,11 @@ public class User {
 	private List<Trip> trips = new ArrayList<Trip>();
 
 	private List<User> friends = new ArrayList<User>();
-	
+
 	public List<User> getFriends() {
 		return friends;
 	}
-	
+
 	public void addFriend(User user) {
 		friends.add(user);
 	}
@@ -22,13 +22,21 @@ public class User {
 	public boolean isFriendsWith(User friend) {
 		return friends.contains(friend);
 	}
-	
+
 	public void addTrip(Trip trip) {
 		trips.add(trip);
 	}
-	
+
 	public List<Trip> trips() {
 		return trips;
 	}
 
+	public boolean isFriend(User usr){
+		for (User friend : this.getFriends()) {
+			if (friend.equals(usr)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
